86a43f77400c5e0c6cbf8090adf5f21c449a086a
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-30 15:15:30 -0500

    more examples of React

---
+++ src/Greet.js
@@ -0,0 +1,13 @@
+//Import React//
+
+import React from 'react';
+
+// Create Function
+
+function Greet ({whom}) {
+return(<p>Hello, {whom}</p>);
+}
+
+//export the function
+
+export default Greet;
